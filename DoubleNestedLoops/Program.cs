d8d84aa30fe05192b6009499d87a602e687ef22b
Robertspoon <robertthemcjug@gmail.com>
2022-11-07 11:08:07 -0400

    xsaafdew

--- DoubleNestedLoops/Program.cs
+++ DoubleNestedLoops/Program.cs
@@ -10,7 +10,23 @@ namespace DoubleNestedLoops
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Double Nested Loop and 2D arrays");
+           // Console.WriteLine("Double Nested Loop and 2D arrays");
+           // Console.WriteLine();
+
+            
+
+
+            for (int i = 0; i <=9; i++)
+            {
+                Console.WriteLine("My misery is immesurable");
+               for(int j = 0; j <= 9; j++)
+                {
+                    Console.WriteLine(i + "," + j);
+                }
+                
+            }
+            
+            
 
             Console.ReadKey(true);
         }
